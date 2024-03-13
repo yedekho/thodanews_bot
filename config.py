d9617eda2f5338bc4699d0e44524ed4d5ac51c62
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2024-03-14 00:52:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://dotequity:Achiadi@123@cluster0.hst9mig.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "dotequity")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
