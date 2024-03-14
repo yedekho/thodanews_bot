f96c7d7d3371ecef949cc520bec800f1cbc0d78c
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2024-03-14 18:45:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Instagram", callback_data = "about"),
-                    InlineKeyboardButton("Telegram", callback_data = "close")
+                    InlineKeyboardButton("Instagram", url=f"instagram.com/wownewss"),
+                    InlineKeyboardButton("Telegram", url=f"telegram.me/wownewsss")
                 ]
             ]
         )
